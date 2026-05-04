18315efb91a8b7ed9984fb636ade5c37b1ce47ee
SamuelJacksonGrim <samgrim97@gmail.com>
2026-05-03 19:57:14 -0600

    Add emotional colour palette dictionary

---
+++ emotional_engine/colour_system/emotional_palette.py
@@ -0,0 +1,10 @@
+EMOTIONAL_COLOURS = {
+    "expansion": "gold",
+    "contraction": "blue",
+    "fragmentation": "neon_cyan",
+    "compression": "dark_red",
+    "sharpness": "crimson",
+    "softening": "lavender",
+    "oscillation": "green_blue_shift",
+    "stillness": "white"
+}
